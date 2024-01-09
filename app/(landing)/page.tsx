03925ba7aebb8d82747a41270aa78f0cd80d5068
Nilay-09 <nilaybhotmange2002@gmail.com>
2024-01-09 20:20:53 +0530

    doneEverything

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,34 @@
-
+"use client"
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+    const [backgroundImage, setBackgroundImage] = useState("/final.jpg");
+
+    useEffect(() => {
+        const handleResize = () => {
+            setBackgroundImage(window.innerWidth <= 600 ? "/sideImg.jpg" : "/final.jpg");
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <div className="h-full ">
             <div className="w-full h-full relative bg-black/70 imageContainer">
                 <Image
                     width={1200}
                     height={700}
-                    src="/final.jpg"
+                    src={backgroundImage}
                     alt="Background"
-                    className="absolute w-screen h-full -z-10"
+                    className="absolute w-screen h-full -z-10 object-cover"
                 />
                 <LandingNavbar />
                 <LandingHero />
