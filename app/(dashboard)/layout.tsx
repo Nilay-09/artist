5a7dd119e17a4206c87529a74e6046d3652bf21c
Nilay-09 <nilaybhotmange2002@gmail.com>
2024-01-09 20:00:47 +0530

    DoneEverything

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ const DashboardLayout = async ({
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
                 <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
             </div>
-            <main className="md:pl-72 pb-10">
+            <main className="md:pl-72 pb-10 polka">
                 <Navbar />
                 <div className="h-[90vh]">
                     {children}
--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -6,7 +6,6 @@ import { Crisp } from "crisp-sdk-web";
 export const CrispChat = () => {
     useEffect(() => {
         Crisp.configure("8333bc7f-d818-46ea-81bb-84658e595a3e");
-        Crisp.setPosition("left");
     }, []);
 
     return null;
