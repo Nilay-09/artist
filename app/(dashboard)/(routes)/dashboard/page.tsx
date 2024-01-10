6108cc6d519a8204714739d297bb1b1a98b53c9b
Nilay-09 <nilaybhotmange2002@gmail.com>
2024-01-10 10:17:29 +0530

    headerDone

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -14,8 +14,8 @@ export default function HomePage() {
     return (
         <div>
             <div className="mb-8 space-y-4">
-                <h2 className="text-2xl md:text-4xl font-bold text-center">
-                    Explore the power of AI
+                <h2 className="text-2xl text-black md:text-4xl font-bold text-center">
+                    Empowering Possibilities Through the Magic of AI
                 </h2>
                 <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
                     Chat with the smartest AI - Experience the power of AI
